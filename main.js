6caff3773834fd49eb30485f071aa6e88ac3a889
GersonTejadaRodrigo <160535805+GersonTejadaRodrigo@users.noreply.github.com>
2024-03-18 21:10:16 -0500

    exportar-excel

---
+++ main.js
@@ -0,0 +1,13 @@
+const $btnExportar = document.querySelector("#btnExportar"),
+    $tabla = document.querySelector("#tabla");
+
+$btnExportar.addEventListener("click", function() {
+    let tableExport = new TableExport($tabla, {
+        exportButtons: false,  
+        filename: "Reporte de prueba",  
+        sheetname: "Reporte de prueba",  
+    });
+    let datos = tableExport.getExportData();
+    let preferenciasDocumento = datos.tabla.xlsx;
+    tableExport.export2file(preferenciasDocumento.data, preferenciasDocumento.mimeType, preferenciasDocumento.filename, preferenciasDocumento.fileExtension, preferenciasDocumento.merges, preferenciasDocumento.RTL, preferenciasDocumento.sheetname);
+});
